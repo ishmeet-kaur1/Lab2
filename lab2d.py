2a13374a29526e005827be527967caade06560ab
ishmeetkaur3 <156566750+ishmeetkaur3@users.noreply.github.com>
2024-02-12 22:24:02 -0500

    Add files via upload

---
+++ lab2d.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+
+# Check if the correct number of arguments is provided
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} name age")
+    sys.exit(1)
+
+# Assign command line arguments to variables
+name = sys.argv[1]
+age = sys.argv[2]
+
+# Print the formatted output
+print(f"Hi {name}, you are {age} years old.")
